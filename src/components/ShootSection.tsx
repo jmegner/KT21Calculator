6266d341d9515b13a10832c787717fd683c40dcb
Jacob Egner <JacobEgner@example.com>
2024-04-13 14:13:29 -0500

    ktshoot situation swiping, first draft
    doesn't accept swipe gesture as well as Daniel's PR; need to figure out why

--- src/components/ShootSection.tsx
+++ src/components/ShootSection.tsx
@@ -4,6 +4,7 @@ import {
   Container,
   Row,
 } from 'react-bootstrap';
+import SwipeableViews from "react-swipeable-views"
 
 import Credits from 'src/components/Credits';
 
@@ -33,28 +34,55 @@ const ShootSection: React.FC = () => {
     N.FireTeamRules,
   ].map(note => <li key={note.name}><b>{note.name}</b>: {note.description}</li>);
 
-  return (
+  const isTouch = window.ontouchstart !== undefined;
+  const safeWidth = window.visualViewport ? window.visualViewport.width : window.innerWidth;
+  const isNarrow = safeWidth < 768; // meaning too narrow to show both situations side by side
+  const shouldUseSwipe = isTouch && isNarrow;
+
+  const situationsRow = shouldUseSwipe
+    ?
+      <Row>
+        <Col className='border p-0'>
+          <SwipeableViews>
+            <div>
+              <h2 className='sticky-top' style={{textAlign:'center'}}>Situation 1</h2>
+              <ShootSituation/>
+            </div>
+            <div>
+              <h2 style={{textAlign:'center'}}>Situation 2</h2>
+              <ShootSituation/>
+            </div>
+          </SwipeableViews>
+        </Col>
+      </Row>
+    :
+      <Row>
+        <Col className='border p-0'>
+          <ShootSituation/>
+        </Col>
+        <Col className='border p-0'>
+          <ShootSituation/>
+        </Col>
+      </Row>
+    ;
+
+  return (<>
+    {`isTouch: ${isTouch}`}<br/>
+    {`width: ${safeWidth}`}<br/>
+    {`isNarrow: ${isNarrow}`}<br/>
+    {`shouldUseSwipe: ${shouldUseSwipe}`}<br/>
     <Container style={{width: 'fit-content'}}>
       <Row>
         Kill Team 2021 Edition, Shooting&nbsp;
         <a href='https://www.warhammer-community.com/wp-content/uploads/2022/08/ekD0GG2pTHlYba0G.pdf'>[Lite Rules]</a>
       </Row>
-      <Row>
-        <Col className='border p-0'>
-          Situation1
-          <ShootSituation/>
-        </Col>
-        <Col className='border p-0'>
-          Situation2
-          <ShootSituation/>
-        </Col>
-      </Row>
+      {situationsRow}
       <Row>
         <Col className={Util.centerHoriz + ' border'} style={{fontSize: '11px'}}>
           <Credits/>
         </Col>
       </Row>
-      <Row style={{width: '800px'}}>
+      <Row>
         <Col className='border' style={{fontSize: '11px'}}>
           Notes:
           <ul>
@@ -63,7 +91,7 @@ const ShootSection: React.FC = () => {
         </Col>
       </Row>
     </Container>
-  );
+  </>);
 };
 
 export default ShootSection;
